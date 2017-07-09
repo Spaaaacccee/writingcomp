201a2153bb3a86961d4627eef76a22404211ef71
Spaaaacccee <kaiwen530@gmail.com>
2017-07-09 21:30:24 +1000

    iOS scrolling fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 var gen = sentenceGenerator(txt);
 var indexedWords = txt.split(" ");
 
-window.onload = function() {
+window.onload = function () {
+
+    //iOS scrolling fix
+    var vpH = window.innerHeight;
+    document.documentElement.style.height = vpH.toString() + "px";
+    body.style.height = vpH.toString() + "px";
+
+
     $('#bg-text')[0].innerHTML = gen.take(410);
-    $('#para-viewport').parallaxify({positionProperty: 'transform',responsive:true,alphaFilter:0.7});
+    $('#para-viewport').parallaxify({
+        positionProperty: 'transform',
+        responsive: true,
+        alphaFilter: 0.7
+    });
 }
