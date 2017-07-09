dac24d954d3853ee7caf81ad75bb1743d07a8137
Spaaaacccee <kaiwen530@gmail.com>
2017-07-09 21:36:36 +1000

    Multiple changes

--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ var indexedWords = txt.split(" ");
 
 window.onload = function () {
 
-    //iOS scrolling fix
-    var vpH = window.innerHeight;
-    document.documentElement.style.height = vpH.toString() + "px";
-    body.style.height = vpH.toString() + "px";
-
-
     $('#bg-text')[0].innerHTML = gen.take(410);
     $('#para-viewport').parallaxify({
         positionProperty: 'transform',
